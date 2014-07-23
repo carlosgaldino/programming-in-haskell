dca6e84d8c7eae8121214d53b5499d3a5bb3bf39
Carlos Galdino <carloshsgaldino@gmail.com>
2014-07-23 13:01:27 -0300

    Add Exercise 4.

--- ch06/Chapter6.hs
+++ ch06/Chapter6.hs
@@ -80,3 +80,11 @@ elem' x [] = False
 elem' x (y:ys)
   | x == y = True
   | otherwise = elem' x ys
+
+-- Exercise 4
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+  | x <= y = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
