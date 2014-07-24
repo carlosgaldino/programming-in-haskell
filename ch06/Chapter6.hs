479601c8643a9d419b1b85c0af777e6e13c197b0
Carlos Galdino <carloshsgaldino@gmail.com>
2014-07-24 07:24:11 -0300

    Add solution for Exercise 5.

--- ch06/Chapter6.hs
+++ ch06/Chapter6.hs
@@ -90,3 +90,12 @@ merge [] ys = ys
 merge (x:xs) (y:ys)
   | x <= y = x : merge xs (y:ys)
   | otherwise = y : merge (x:xs) ys
+
+-- Exercise 5
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+msort :: Ord a => [a] -> [a]
+msort [x] = [x]
+msort xs = merge (msort l) (msort r)
+  where (l, r) = halve xs
