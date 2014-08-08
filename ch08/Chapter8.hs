ce9d866fe546ebe0c645acad3e7ad83abb6d23e5
Carlos Galdino <carloshsgaldino@gmail.com>
2014-08-08 01:28:41 -0300

    Add exercises 6, 7 and 8 of chapter 8.

--- ch08/Chapter8.hs
+++ ch08/Chapter8.hs
@@ -2,8 +2,7 @@ module Chapter8 where
 
 import Parsing
 
--- Since `int` is not defined inside `Parsing` we need to define `integer`
--- here.
+-- Since `int` is not defined inside `Parsing` we need to define `integer` here.
 integer :: Parser Int
 integer = token int
 
@@ -19,3 +18,103 @@ comment :: Parser ()
 comment = do symbol "--"
              many (sat (/= '\n'))
              return ()
+
+-- Exercise 6
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+           +++ do symbol "-"
+                  e <- expr
+                  return (t - e)
+           +++ return t
+
+term :: Parser Int
+term = do f <- factor
+          do symbol "*"
+             t <- term
+             return (f * t)
+           +++ do symbol "/"
+                  t <- term
+                  return (f `div` t)
+           +++ return f
+
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+          +++ natural
+
+eval :: String -> Int
+eval xs = case parse expr xs of
+            [(n, [])] -> n
+            [(_, out)] -> error ("unused input " ++ out)
+            [] -> error "invalid input"
+
+-- Exercise 7
+factor' :: Parser Int
+factor' = do a <- atom
+             do symbol "^"
+                f <- factor'
+                return (a ^ f)
+              +++ return a
+
+atom :: Parser Int
+atom = do symbol "("
+          e <- expr
+          symbol ")"
+          return e
+        +++ natural
+
+expr' :: Parser Int
+expr' = do t <- term'
+           do symbol "+"
+              e <- expr'
+              return (t + e)
+            +++ do symbol "-"
+                   e <- expr'
+                   return (t - e)
+            +++ return t
+
+term' :: Parser Int
+term' = do f <- factor'
+           do symbol "*"
+              t <- term'
+              return (f * t)
+            +++ do symbol "/"
+                   t <- term'
+                   return (f `div` t)
+            +++ return f
+
+eval' :: String -> Int
+eval' xs = case parse expr' xs of
+             [(n, [])] -> n
+             [(_, out)] -> error ("unused input " ++ out)
+             [] -> error "invalid input"
+
+-- Exercise 8
+expr'' :: Parser Int
+expr'' = do e <- expr''
+            symbol "-"
+            n <- natural
+            return (e - n)
+          +++ natural
+
+{--
+The problem with this parser is that it loops forever without ever producing a
+result of its left part that is being called recursively.
+--}
+
+expr''' :: Parser Int
+expr''' = do n <- natural
+             ns <- many (do symbol "-"
+                            natural)
+             return (foldl (-) n ns)
+
+eval'' :: String -> Int
+eval'' xs = case parse expr''' xs of
+              [(n, [])] -> n
+              [(_, out)] -> error ("unused input " ++ out)
+              [] -> error "invalid input"
