7b94db50258bf442b0488a0843eebdf49f3bf8db
Carlos Galdino <carloshsgaldino@gmail.com>
2014-09-21 20:18:41 -0300

    Add remaining exercises from Chapter 11.

--- ch11/Chapter11.hs
+++ ch11/Chapter11.hs
@@ -31,3 +31,61 @@ isChoice _ [] = False
 isChoice (x:xs) ys
   | x `elem` ys = isChoice xs (removeOne x ys)
   | otherwise   = False
+
+data Op = Add | Sub | Mul | Div | Exp
+
+data Expr = Val Int | App Op Expr Expr
+
+valid :: Op -> Int -> Int -> Bool
+valid Add _ _ = True
+valid Sub x y = True
+valid Mul _ _ = True
+valid Div x y = y /= 0 && x `mod` y == 0
+valid Exp _ _ = True
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
+apply Exp x y = x ^ y
+
+split :: [a] -> [([a],[a])]
+split []     = []
+split [_]    = []
+split (x:xs) = ([x],xs) : [(x:ls,rs) | (ls,rs) <- split xs]
+
+exprs :: [Int] -> [Expr]
+exprs []  = []
+exprs [n] = [Val n]
+exprs ns  = [e | (ls,rs) <- split ns
+               , l       <- exprs ls
+               , r       <- exprs rs
+               , e       <- combine l r]
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+
+ops :: [Op]
+ops = [Add, Sub, Mul, Div, Exp]
+
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <- choices ns
+                    , e   <- exprs ns'
+                    , eval e == [n]]
+
+eval :: Expr -> [Int]
+eval (Val n)     = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l
+                                , y <- eval r
+                                , valid o x y]
+
+-- Exercise 4 and 5
+main :: IO ()
+main = do
+    putStrLn . show $ length [e | ns <- choices [1, 3, 7, 10, 25, 50], e <- exprs ns]
+    -- 33665406
+    putStrLn . show $ length [e | ns <- choices [1, 3, 7, 10, 25, 50], e <- exprs ns, eval e /= []]
+    -- 4672540
+    putStrLn . show $ length [e | ns <- choices [1, 3, 7, 10, 25, 50], e <- exprs ns, eval e /= []]
+    -- 10839369
